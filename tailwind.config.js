e07357553da7c1b01b9db9eb482e823eac553568
jskfox <solanokirk3@hotmail.com>
2024-11-19 01:20:32 -0800

    updating pageSpeed Insight recomendations

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
       colors: {
         primary: 'var(--primary)',
         secondary: 'var(--secondary)',
+        terceary: 'var(--terceary)',
       },
       animation: {
         'float': 'float 6s ease-in-out infinite',
