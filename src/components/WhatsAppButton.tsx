53dc62245962201af1eea6876f27c320dbc8ef1f
jskfox <solanokirk3@hotmail.com>
2024-11-19 01:32:19 -0800

    updating pageSpeed Insight recomendations 2nd round

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 import { MessageCircle } from 'lucide-react';
 
@@ -12,7 +11,7 @@ const WhatsAppButton = () => {
   };
 
   return (
-    <motion.button
+    <motion.div
       onClick={handleClick}
       className="fixed bottom-6 right-6 z-50 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition-colors"
       whileHover={{ scale: 1.1 }}
@@ -21,7 +20,7 @@ const WhatsAppButton = () => {
       animate={{ opacity: 1, y: 0 }}
     >
       <MessageCircle size={28} />
-    </motion.button>
+    </motion.div>
   );
 };
 
