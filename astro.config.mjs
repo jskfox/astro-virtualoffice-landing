91eb8f650010bb38091fa7f1742dd7ca767de884
jskfox <solanokirk3@hotmail.com>
2024-12-09 14:46:45 -0800

    adding Google Tag support with PartyTown for better performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+import partytown from '@astrojs/partytown';
+
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), partytown()],
   site: import.meta.env.SITE_URL,
 });
---
+++ src/utils/gtag.js
@@ -0,0 +1,4 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', import.meta.env.PUBLIC_GTAG_ID);
